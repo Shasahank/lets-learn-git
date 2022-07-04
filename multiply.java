8c94a6757c650266e767a9f5ee0b38bb1eee04c9
Shashank Sakhre <shashanksakhre02@gmail.com>
2022-07-04 16:48:51 +0530

    adding multiply to shashank/multiply branch

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply {
+    public int multi(int a,int b){
+        return (a*b);
+    }
+    public static void main(String[] args) {
+        multiply d=new multiply();
+        d.multi(5, 3);
+    }
+}
