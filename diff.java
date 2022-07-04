76667f066a9d0f5495ce8e50781c3d8f676893c6
Shashank Sakhre <shashanksakhre02@gmail.com>
2022-07-04 17:14:17 +0530

    added implementaion in diff.java

--- diff.java
+++ diff.java
@@ -1,10 +1,4 @@
 public class diff {
    
-    public int difference(int a,int b){
-        return (a-b);
-    }
-    public static void main(String[] args) {
-        diff d=new diff();
-        d.difference(5, 3);
-    }
+    
 }
